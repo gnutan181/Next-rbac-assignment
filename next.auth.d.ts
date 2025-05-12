e9cfdf7096d17e2b02f6f5f2249870441de0eb63
gnutan <gnutan181@gmail.com>
2025-05-12 21:50:58 +0530

    setup for hosting

--- next.auth.d.ts
+++ next.auth.d.ts
@@ -1,13 +1,34 @@
 // src/types/next-auth.d.ts
 import { DefaultSession } from 'next-auth';
+import { JWT as DefaultJWT } from 'next-auth/jwt'
+export type UserRole = 'USER' | 'ADMIN'
 
 declare module 'next-auth' {
   interface Session extends DefaultSession {
     user?: {
       id: string;
-      name: string;
-      email: string;
-      role: 'USER' | 'ADMIN';
-    };
+      name?: string | null;
+      email?: string | null;  
+      role: string
+    } & DefaultSession['user']
+  }
+  interface User {
+    id: string
+    name?: string | null
+    email?: string | null
+    role: UserRole
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT extends DefaultJWT {
+    id: string
+    role: string
+    email: string
+    name: string
+    sub: string
+    iat: number
+    exp: number
+    jti: string
   }
 }
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,15 @@
 // src/app/api/auth/[...nextauth]/route.ts
-import NextAuth,{SessionStrategy} from 'next-auth';
+import NextAuth,{SessionStrategy, Session,AuthOptions} from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { PrismaClient } from '@prisma/client';
 import { compare } from 'bcryptjs';
 // import type { Session, JWT } from 'inspector/promises';
+import { JWT } from 'next-auth/jwt'
+import type { User } from 'next-auth'
+
 const prisma = new PrismaClient();
 
-export const authOptions = {
+export const authOptions:AuthOptions = {
   providers: [
     Credentials({
       name: 'credentials',
@@ -14,7 +17,7 @@ export const authOptions = {
         email: { label: 'Email', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
-      async authorize(credentials) {
+      async authorize(credentials):Promise<User | null> {
         if (!credentials?.email || !credentials?.password) {
           throw new Error('Email and password are required');
         }
@@ -22,7 +25,7 @@ export const authOptions = {
         const user = await prisma.user.findUnique({
           where: { email: credentials.email },
         });
-
+      // console.log(user,"in")
         if (!user || !(await compare(credentials.password, user.password))) {
           throw new Error('Invalid email or password');
         }
@@ -31,32 +34,41 @@ export const authOptions = {
           id: user?.id,
           name: user?.name,
           email: user?.email,
-          role: user?.role,
+          role: user?.role as 'USER' | 'ADMIN',
         };
       },
     }),
   ],
   callbacks: {
-    jwt: async ({ token, user }) => {
-      console.log(typeof token, 'token')
+    // jwt: async ({ token, user }) => {
+      jwt: async ({ token, user }: { token: JWT; user: User | null }) => {
 
+//       console.log(typeof token, '')
+// console.log(user, 'usfger')
       
       if (user) {
         token.id = user?.id;
         token.role = user?.role;
       }
+      console.log("token", token)
       return token;
     },
-    session: async ({ session, token }) => {
-      if (token) {
+    // session: async ({ session, token }) => {
+      session: async ({ session, token }: { token:JWT,session: Session;  }) => {
+// console.log(token)
+      if (session.user && token) {
         session.user.id = token?.id;
         session.user.role = token?.role;
+        session.user.name = token.name;
+        session.user.email = token.email;
       }
       return session;
     },
   },
   session: {
     strategy:'jwt' as SessionStrategy,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
