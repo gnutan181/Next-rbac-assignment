c6e3cb4eee3c6b2d95f5fec9d50c561fc9d48f09
gnutan <gnutan181@gmail.com>
2025-05-12 17:25:51 +0530

    setup for hosting

--- src/app/api/admin/users/[id]/route.tsx
+++ src/app/api/admin/users/[id]/route.tsx
@@ -7,8 +7,10 @@ import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export async function PUT(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
+  // { params }: { params: { id: string } }
 ) {
+  const { params } = context; 
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.id || session.user.role !== 'ADMIN') {
